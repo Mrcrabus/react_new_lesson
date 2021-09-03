d4a903e06cab7eb10dba86aa9ba28968e139bb2e
svonk <example@mail.ru>
2021-09-03 15:28:56 +0300

    First commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <form>
+            <div className="form-group">
+                <input type="text" placeholder='Write something' className="form-control"/>
+            </div>
+
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='container-fluid bg-dark text-white rounded-3 p-5'>
+            <div className='container'>
+                <h1 className='display-4'>The best App</h1>
+                <p className='lead'>Version App is 1.0.2</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
